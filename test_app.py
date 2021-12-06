70648fc826bb7031828594a7ee12d2a53c19409d
Gift Chimphonda <gift.chimphonda@outlook.com>
2021-12-06 16:27:59 +0200

    Started Unit Test

--- test_app.py
+++ test_app.py
@@ -1,1 +1,12 @@
+import unittest
+import os
+import json
 
+class Test_Raffle(unittest.TestCase):
+    def setup(self):
+
+    def test_raffle(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
